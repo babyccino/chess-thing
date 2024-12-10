5c4d11f79a88f7d36c4f09dbfddc8bf66b60473e
Gus Ryan <gus.ryan163@gmail.com>
2024-12-10 00:34:12 +0000

    wip

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
-import boardPkg "chess/board"
+import boardState "chess/board"
 
 func main() {
-	board := boardPkg.NewBoard()
+	board := boardState.NewBoard()
 	println("before:\n")
 	board.Print()
-	move1, _ := boardPkg.StringToPosition("A5")
-	move2, _ := boardPkg.StringToPosition("A6")
+
+	move1, _ := boardState.StringToPosition("A5")
+	move2, _ := boardState.StringToPosition("A6")
 	board.Move(move1, move2)
 	println("after:\n")
 	board.Print()
+	println(board.Fen())
+
+	fen := "KRBPP3/RQKP4/KBP5/PP5p/6pp/P4pbk/4pkqr/3ppbrk w"
+	fenBoard, err := boardState.ParseFen(fen)
+	fenBoard.Print()
 }
