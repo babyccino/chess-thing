9b3cf11e980bebad5f146e653d465cbbe2cf2dd2
Gus Ryan <gus.ryan163@gmail.com>
2025-01-11 02:29:35 +0000

    add basic game ui

--- web/src/components/Game.svelte
+++ web/src/components/Game.svelte
@@ -1,5 +1,82 @@
 <script lang="ts">
-  // instance-level logic goes here
+  import Icon from "@iconify/svelte";
+  import {
+    type Piece,
+    indexToFile,
+    indexToRow,
+    initialBoardState,
+    pieceToIcon,
+    serialiseMove,
+  } from "../library/board";
+
+  let messages = $state<any[]>([]);
+
+  let ws: WebSocket = new WebSocket("ws://localhost:3000/subscribe/hello");
+
+  ws.addEventListener("open", (event) => {
+    console.log("open:", event);
+  });
+
+  ws.addEventListener("message", (event) => {
+    console.log("received:", event);
+    messages.push(event.data);
+  });
+
+  let board = $state<Piece[]>(initialBoardState);
+  let colour = $state(true);
+  let displayBoard = $derived(colour ? board.toReversed() : board);
+  let selected = $state<number | null>(null);
+
+  function fromDisplayIndex(index: number) {
+    return colour ? 63 - index : index;
+  }
+
+  function handleClickPiece(square: number) {
+    console.log("clicked", square);
+    if (selected === null) {
+      selected = square;
+    } else {
+      ws.send(serialiseMove(selected, square));
+      selected = null;
+    }
+  }
+
+  function isBlack(index: number): boolean {
+    const rowOffset = Math.floor(index / 8);
+    return Boolean((index + rowOffset) % 2);
+  }
 </script>
 
-<!-- markup (zero or more items) goes here -->
+<ul>
+  {#each messages as message}
+    <li>{message}</li>
+  {/each}
+</ul>
+
+<div class="grid grid-cols-8">
+  {#each displayBoard as piece, index}
+    <button
+      class={[
+        "w-16 h-16 flex justify-center items-center relative",
+        {
+          "bg-gray-700": isBlack(fromDisplayIndex(index)),
+          "bg-gray-200": !isBlack(fromDisplayIndex(index)),
+          "border-2 border-red-100": index === selected,
+        },
+      ]}
+      onclick={() => handleClickPiece(fromDisplayIndex(index))}
+    >
+      <Icon class="w-10 h-10" icon={pieceToIcon(piece)} />
+      {#if index % 8 === 0}
+        <p class="absolute top-0 left-0 text-xs text-red-200">
+          {indexToRow(fromDisplayIndex(index))}
+        </p>
+      {/if}
+      {#if index >= 56}
+        <p class="absolute bottom-0 right-[0.1rem] text-xs text-red-200">
+          {indexToFile(fromDisplayIndex(index))}
+        </p>
+      {/if}
+    </button>
+  {/each}
+</div>
---
+++ web/src/library/board.ts
@@ -0,0 +1,127 @@
+export type Piece = number;
+
+export const Clear = 0;
+export const WKing = 1;
+export const WQueen = 2;
+export const WBishop = 3;
+export const WKnight = 4;
+export const WPawn = 5;
+export const WRook = 6;
+export const BKing = 7;
+export const BQueen = 8;
+export const BBishop = 9;
+export const BKnight = 10;
+export const BPawn = 11;
+export const BRook = 12;
+
+export const piecesToIcon = [
+  "",
+  "fa6-solid:chess-king",
+  "fa6-solid:chess-queen",
+  "fa6-solid:chess-bishop",
+  "fa6-solid:chess-knight",
+  "fa6-solid:chess-pawn",
+  "fa6-solid:chess-rook",
+  "fa6-regular:chess-king",
+  "fa6-regular:chess-queen",
+  "fa6-regular:chess-bishop",
+  "fa6-regular:chess-knight",
+  "fa6-regular:chess-pawn",
+  "fa6-regular:chess-rook",
+] as const;
+
+export function pieceToIcon(piece: Piece) {
+  return piecesToIcon[piece];
+}
+
+export const initialBoardState = [
+  WKing,
+  WRook,
+  WBishop,
+  WPawn,
+  WPawn,
+  Clear,
+  Clear,
+  Clear,
+  WRook,
+  WQueen,
+  WKnight,
+  WPawn,
+  Clear,
+  Clear,
+  Clear,
+  Clear,
+  WKnight,
+  WBishop,
+  WPawn,
+  Clear,
+  Clear,
+  Clear,
+  Clear,
+  Clear,
+  WPawn,
+  WPawn,
+  Clear,
+  Clear,
+  Clear,
+  Clear,
+  Clear,
+  BPawn,
+  WPawn,
+  Clear,
+  Clear,
+  Clear,
+  Clear,
+  Clear,
+  BPawn,
+  BPawn,
+  Clear,
+  Clear,
+  Clear,
+  Clear,
+  Clear,
+  BPawn,
+  BBishop,
+  BKnight,
+  Clear,
+  Clear,
+  Clear,
+  Clear,
+  BPawn,
+  BKnight,
+  BQueen,
+  BRook,
+  Clear,
+  Clear,
+  Clear,
+  BPawn,
+  BPawn,
+  BBishop,
+  BRook,
+  BKing,
+];
+
+export function padNumber(num: number): string {
+  if (num < 10) return `0${num}`;
+  return num.toString();
+}
+
+export function serialiseMove(from: number, to: number): string {
+  return padNumber(from) + padNumber(to);
+}
+
+const aChar = "A".charCodeAt(0);
+const zeroChar = "1".charCodeAt(0);
+export function indexToFile(index: number): string {
+  const x = index % 8;
+  return String.fromCharCode(aChar + 7 - x);
+}
+export function indexToRow(index: number): string {
+  const y = index / 8;
+  return String.fromCharCode(zeroChar + y);
+}
+export function indexToPosition(index: number): string {
+  const y = index / 8;
+  const x = index % 8;
+  return indexToFile(index) + indexToRow(index);
+}
