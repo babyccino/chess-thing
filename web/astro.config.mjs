88ee1abd2a23d9279bea018dcf4fa6edda2d9c6e
Gus Ryan <gus.ryan163@gmail.com>
2025-01-12 00:39:19 +0000

    add prettier

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte"
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind"
 
-import icon from 'astro-icon';
+import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), icon()]
-});
+  integrations: [svelte(), tailwind(), icon()],
+  // TODO I guess maybe not cos I'm gonna build static and serve
+  vite: { server: { cors: { origin: "*" } } },
+})
--- web/src/library/board.ts
+++ web/src/library/board.ts
@@ -1,18 +1,18 @@
-export type Piece = number;
+export type Piece = number
 
-export const Clear = 0;
-export const WKing = 1;
-export const WQueen = 2;
-export const WBishop = 3;
-export const WKnight = 4;
-export const WPawn = 5;
-export const WRook = 6;
-export const BKing = 7;
-export const BQueen = 8;
-export const BBishop = 9;
-export const BKnight = 10;
-export const BPawn = 11;
-export const BRook = 12;
+export const Clear = 0
+export const WKing = 1
+export const WQueen = 2
+export const WBishop = 3
+export const WKnight = 4
+export const WPawn = 5
+export const WRook = 6
+export const BKing = 7
+export const BQueen = 8
+export const BBishop = 9
+export const BKnight = 10
+export const BPawn = 11
+export const BRook = 12
 
 export const piecesToIcon = [
   "",
@@ -28,10 +28,10 @@ export const piecesToIcon = [
   "fa6-regular:chess-knight",
   "fa6-regular:chess-pawn",
   "fa6-regular:chess-rook",
-] as const;
+] as const
 
 export function pieceToIcon(piece: Piece) {
-  return piecesToIcon[piece];
+  return piecesToIcon[piece]
 }
 
 export const initialBoardState = [
@@ -99,29 +99,29 @@ export const initialBoardState = [
   BBishop,
   BRook,
   BKing,
-];
+]
 
 export function padNumber(num: number): string {
-  if (num < 10) return `0${num}`;
-  return num.toString();
+  if (num < 10) return `0${num}`
+  return num.toString()
 }
 
 export function serialiseMove(from: number, to: number): string {
-  return padNumber(from) + padNumber(to);
+  return padNumber(from) + padNumber(to)
 }
 
-const aChar = "A".charCodeAt(0);
-const zeroChar = "1".charCodeAt(0);
+const aChar = "A".charCodeAt(0)
+const zeroChar = "1".charCodeAt(0)
 export function indexToFile(index: number): string {
-  const x = index % 8;
-  return String.fromCharCode(aChar + 7 - x);
+  const x = index % 8
+  return String.fromCharCode(aChar + 7 - x)
 }
 export function indexToRow(index: number): string {
-  const y = index / 8;
-  return String.fromCharCode(zeroChar + y);
+  const y = index / 8
+  return String.fromCharCode(zeroChar + y)
 }
 export function indexToPosition(index: number): string {
-  const y = index / 8;
-  const x = index % 8;
-  return indexToFile(index) + indexToRow(index);
+  const y = index / 8
+  const x = index % 8
+  return indexToFile(index) + indexToRow(index)
 }
